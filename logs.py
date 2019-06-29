4ce8607cc43a3457c5f8addac71b7e8793dbc036
dailing <qzyz_dailing@163.com>
2019-06-30 00:16:53 +0800

    change logging file behavier

--- logs.py
+++ logs.py
@@ -18,6 +18,7 @@ def get_logger(name: str, print_level=logging.DEBUG):
         os.makedirs(f'log/{time_now}/', exist_ok=True)
     file_handler = logging.FileHandler(f'log/{time_now}/{name}.log')
     file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
     logger.addHandler(stream_handler)
     logger.addHandler(file_handler)
     logger.setLevel(logging.DEBUG)
